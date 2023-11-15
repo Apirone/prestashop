454e67bcfa6661225729fe73802652a488121bfb
alex <alex.zaytseff@gmail.com>
2023-11-15 18:49:28 +0400

    Updated file header template

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,11 +1,17 @@
 <?php
 
 $header = <<<TXT
-Package: Prestashop Apirone Payment gateway
+2017-2023 apirone.com
 
-Another header line 1
-Another header line 2
+DISCLAIMER
 
+Do not edit or add to this file if you wish to upgrade AmazonPay to newer
+versions in the future. If you wish to customize PrestaShop for your
+needs please refer to http://www.prestashop.com for more information.
+
+ @author    Apirone OÜ <support@apirone.com>
+ @copyright 2017-2023 Apirone OÜ
+ @license   http://opensource.org/licenses/afl-3.0.php  The MIT License
 TXT;
 
 $finder = PhpCsFixer\Finder::create()
